c208c971b2ded5e711271fa9b984234bde94fe65
myokoym <myokoym@gmail.com>
2012-08-23 22:19:00 +0900

    最低限の実装 [implementation of the minimum]

--- lib/daberu.rb
+++ lib/daberu.rb
@@ -1,5 +1,13 @@
-require "daberu/version"
+# encoding: utf-8
 
-module Daberu
-  # Your code goes here...
+class Daberu
+  def initialize(obj = Object.new)
+    @obj = obj
+  end
+
+  def method_missing(name, *args, &block)
+    p "Method: #{name}, Arguments: #{args}, Block: #{block}"
+    @obj.send(name, *args, &block)
+  end
 end
+
